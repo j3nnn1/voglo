789cea47f9ec195007796c6d78aa0057084882f3
Jennifer <jennifer.maldonado31415@gmail.com>
2019-03-16 12:12:50 -0700

    principal scene, the road to deliver orders, box collider to not fall.

---
+++ voglo/Assets/Scripts/ControllerPlayer.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerPlayer : MonoBehaviour
+{
+    Rigidbody rb;
+    public float velocity = 0.5f;
+    public bool localX = true;
+    public bool localZ = true;
+    public bool sobrePlano = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.forward * Time.deltaTime * velocity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rb == null)
+            return;
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 right, forward;
+        if (localX)
+            right = transform.right;
+        else
+            right = Vector3.right;
+
+        if (localZ)
+            forward = transform.forward;
+        else
+            forward = Vector3.forward;
+
+        if (sobrePlano)
+        {
+            right.y = 0;
+            forward.y = 0;
+            right.Normalize();
+            forward.Normalize();
+        }
+
+        rb.velocity = (h * Vector3.right + v * Vector3.forward) * velocity + Vector3.up * rb.velocity.y;
+        //1 transform.position += transform.forward * Time.deltaTime;
+        //2 transform.Translate(Vector3.left * Time.deltaTime);
+        // 3 NO ANDA rb.AddForce(transform.forward * velocity * Time.deltaTime);
+        // 4 rb.MovePosition(transform.position + (transform.forward * Time.deltaTime));
+        // 4 alternative Vector3 newPosition = transform.position + (transform.forward * Time.deltaTime);
+        // 4 rb.MovePosition(newPosition);
+
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        //col.transform.position = new Vector3(0, -5f, 0);
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            //Vector3 newPos = transform.position;
+            //Vector3 collisionDir  = rb.ClosestPointOnBounds(collision.transform.position);
+            //Debug.Log("Desde el jugador: y el objecto 2 es una pared ");
+            //Debug.Log(newPos);
+            //Debug.Log(collisionDir);
+            //transform.position = collisionDir;
+            rb.velocity = new Vector3(0, 0, 0);
+        }
+
+    }
+    //private void OnCollisionExit(Collision collision)
+    //{
+    //    if (collision.gameObject.CompareTag("Wall"))
+    //   {
+    //        Vector3 newPos = transform.position;
+    //        Vector3 collisionDir = rb.ClosestPointOnBounds(collision.transform.position);
+    //        Debug.Log("Desde el jugador: y el objecto 2 es una pared ");
+    //       Debug.Log(newPos);
+    //        collisionDir.x = collisionDir.x - 0.3f;
+    //        //when use x plus or lesss
+    //        Debug.Log(collisionDir);
+    //        transform.position = collisionDir;
+
+      //  }
+    //}
+
+}
---
+++ voglo/Assets/Scripts/DetectHit.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectHit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void isTriggerEnter(Collision col)
+    {
+        Debug.Log("Desde la pared isTriggerEnter : se pego con la pared");
+        Debug.Log(col);
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Desde la pared: se pego con la pared un objeto player ");
+        }
+        //col.transform.position = new Vector3(0, -5f, 0);
+    }
+}
